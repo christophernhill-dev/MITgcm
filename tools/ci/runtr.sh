2c26c3e77439e01892b1f5941ccecea8fe52d50c
chris hill <chrishill@dhcp-18-111-92-40.dyn.mit.edu>
2018-01-18 18:52:59 -0500

    more travis

--- tools/ci/runtr.sh
+++ tools/ci/runtr.sh
@@ -4,5 +4,7 @@
 # - called from .travis.yml script: section with environment variables 
 #   already set.
 #
+env
+set
 ${MITGCM_DECMD} "cd /MITgcm/verification; ./testreport -t ${MITGCM_EXP} ${MITGCM_TROPT} | tee ${MITGCM_EXP}/testreport_out.txt"
 ${MITGCM_DECMD} "cd /MITgcm/verification; python verification_parser.py -filename ${MITGCM_EXP}/testreport_out.txt -threshold ${MITGCM_PRECS}"
