4c5fd59ec74f62591c39e8b533ca680cd0dc95e0
chris hill <chrishill@dhcp-18-111-92-131.dyn.mit.edu>
2018-01-19 15:13:03 -0500

    Adding -q setting

--- tools/ci/runtr.sh
+++ tools/ci/runtr.sh
@@ -7,5 +7,5 @@
 # MITGCM_EXP   - MITgcm test to run
 # MITGCM_TROPT - Test report options
 #
-${MITGCM_DECMD} "cd /MITgcm/verification; touch stdout.txt; ./testreport -t ${MITGCM_EXP} ${MITGCM_TROPT} | tee ${MITGCM_EXP}/testreport_out.txt | tee -a stdout.txt"
+${MITGCM_DECMD} "cd /MITgcm/verification; ./testreport -t ${MITGCM_EXP} ${MITGCM_TROPT} | tee ${MITGCM_EXP}/testreport_out.txt"
 ${MITGCM_DECMD} "cd /MITgcm/verification; python verification_parser.py -filename ${MITGCM_EXP}/testreport_out.txt -threshold ${MITGCM_PRECS}"
