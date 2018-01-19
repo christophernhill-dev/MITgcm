80a700b47eb55db5a2946a1150864376f0018a46
chris hill <chrishill@dhcp-18-111-92-131.dyn.mit.edu>
2018-01-19 14:35:12 -0500

    Add single output file that collects all testreport outputs

--- tools/ci/runtr.sh
+++ tools/ci/runtr.sh
@@ -3,8 +3,9 @@
 # Test report running script for Travis CI 
 # - called from .travis.yml script: section with environment variables 
 #   already set.
+# MITGCM_DECMD - command to run under Docker
+# MITGCM_EXP   - MITgcm test to run
+# MITGCM_TROPT - Test report options
 #
-env
-set
-${MITGCM_DECMD} "cd /MITgcm/verification; ./testreport -t ${MITGCM_EXP} ${MITGCM_TROPT} | tee ${MITGCM_EXP}/testreport_out.txt"
+${MITGCM_DECMD} "cd /MITgcm/verification; touch stdout.txt; ./testreport -t ${MITGCM_EXP} ${MITGCM_TROPT} | tee ${MITGCM_EXP}/testreport_out.txt | tee -a stdout.txt"
 ${MITGCM_DECMD} "cd /MITgcm/verification; python verification_parser.py -filename ${MITGCM_EXP}/testreport_out.txt -threshold ${MITGCM_PRECS}"
